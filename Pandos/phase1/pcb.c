b300a44d25fdfa0e84431bd4835cac04264c2796
Amy Kelley <amykelley2222@yahoo.com>
2020-09-02 15:30:17 -0400

    added code to allocPcb

--- Pandos/phase1/pcb.c
+++ Pandos/phase1/pcb.c
@@ -10,11 +10,13 @@ void freePcb(pcb_t*p){
 
 //Gives needed Pcb, then removes Pcb from the list (taken)
 pcb_t* allocPcb(){
-    if(){//if the pcbFree list is empty, return NULL
+    if(emptyProcQ(tp) == TRUE){//if the pcbFree list is empty, return NULL
         return NULL;
     }//otherwise remove element from pcb list, then return a pointer to the removed element
-    //remember that pcbs are reused, so make sure no value is in the pcb when its relocated, so if relocated -> remove all in list?
+    //remember that pcbs are reused, so make sure no value is in the pcb when its relocated, so -> set proprieties to NULL
+    removeProcQ(*tp);
 }
+
 //intilize Pcb list
 void initPcbs(){
     pcbFree_h = mkEmptyProcQ();
@@ -23,14 +25,17 @@ void initPcbs(){
         insertProcQ(&pcbFree_h,&foo[i]);
     }
 }
+
 //Makes list empty :) like my soul
 pcb_t* mkEmptyProcQ(){
     return (NULL);
 }
+
 //This method returns true if the tp is empty, otherwise: returns false
 int emptyProcQ(pcb_t*tp){
     return(tp == NULL);
 }
+
 //This method inserts an element at the front of the queue
 void insertProcQ(pcb_t**tp, pcb_t*p){
     if(emptyProcQ(tp) == TRUE){
@@ -43,6 +48,7 @@ void insertProcQ(pcb_t**tp, pcb_t*p){
     p->tp; //have the new tail point back
     tp = p; //assign tp to the new tail
 }
+
 //This method removes the element at the front of the queue
 pcb_t*removeProcQ(pcb_t**tp){
     if(emptyProcQ(tp)==TRUE){//if there is nothing
@@ -57,6 +63,7 @@ pcb_t*removeProcQ(pcb_t**tp){
     tp = tp->p_prev;//make the second last the new tail
     //get rid of the tail (add a return as well)
 }
+
 //Points to something and that gets removed
 pcb_t*outProcQ(pcb_t**tp, pcb_t*p){
     if(tp == p){
@@ -66,6 +73,7 @@ pcb_t*outProcQ(pcb_t**tp, pcb_t*p){
     (p->p_next) -> (p->p_previous); //have the next point to the previous
     //get rid of the thing (add a return)
 }
+
 //Gives head from Queue, or returns null if empty
 pcb_t*headProcQ(pcb_t*tp){
     if(emptyProcQ(tp))
@@ -73,10 +81,12 @@ pcb_t*headProcQ(pcb_t*tp){
 
     return(tp->p_next);
 }
+
 //This method returns true if the pcb that is pointed to by p has no children, otherwise returns false.
 int emptyChild(pcb_t*p){
     return (p->p_child == NULL);
 }
+
 //This method makes the pcb pointed to by p a child of the parent
 void insertChild(pcb_t*prnt, pcb_t*p){
     if(emptyChild(prnt) == TRUE){ //if no other children, point at new child
@@ -89,6 +99,7 @@ void insertChild(pcb_t*prnt, pcb_t*p){
     prnt -> p_child;
     p -> prnt;
 }
+
 //This method removes the first child returns NULL if no children, otherwise returns pointer to this removed child
 pcb_t* removeChild(pcb_t*p){//where does this point to parent or child?
     if(emptyChild(p)){
@@ -107,6 +118,7 @@ pcb_t* removeChild(pcb_t*p){//where does this point to parent or child?
     //have the parent point to the new pointer
 
 }
+
 //This method makes a child an orphan, and will become a subtree if it has children
 pcb_t*outChild(pcb_t*p){
 
