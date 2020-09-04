ce57fbc5a85d74b1a7a246e7f96b4ef130c7b601
joshiu <joshiu@xavier.edu>
2020-09-03 22:02:28 -0400

    removed support_t statement

--- Pandos/h/types.h
+++ Pandos/h/types.h
@@ -25,7 +25,6 @@ struct pcb_t			*p_next,		/*pointer to next entry*/
 						*p_sib_next; /*pointer to next sibling*/
 						*p_sib_prev; /*pointer to previous sibling*/
 /*process status information*/
-state_t             p_s;         /*processor state*/
 cpu_t               p_time;      /*cpu time used by proc*/
 int 				*p_semAdd;   /*pointer to sema4 on*/
 								/*which process blocked*/
