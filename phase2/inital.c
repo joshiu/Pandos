30f486431e40c45d9129c684da78dd9f6671a30d
joshiu <joshiu@xavier.edu>
2020-09-20 15:23:04 -0400

    blocked some of initial

---
+++ phase2/inital.c
@@ -0,0 +1,10 @@
+/** contains the main
+ * initPcb
+ * initASL
+ * readyQ = mkemptyQ()
+ * set current Proc
+ * scheduler() <-- Last line of the code
+ * 
+ * all the global variables are to be defined and initialized here
+ * initialize all 40 semaphores to 0 (use 1 process and put it on readyQ)
+ * */
