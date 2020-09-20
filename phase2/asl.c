d630144f2ed167321c5251f17be5222d4c8674ad
joshiu <joshiu@xavier.edu>
2020-09-20 15:14:02 -0400

    changed file structure and added phase 2

---
+++ phase2/asl.c
@@ -0,0 +1,164 @@
+#include "../h/const.h"
+#include "../h/types.h"
+#include "../h/asl.h"
+#include "../h/pcb.h"
+
+
+/**
+ * This file contains the ASL or active semphone list. Which has public and private methods that are excessors and mutators that
+ * manipulates this data structure.
+ * 
+ * Written by Umang Joshi and Amy Kelley with help from Mikey G. 
+ **/
+
+/*semaphore descriptor type */
+typedef struct semd_t {
+    struct semd_t   *s_next;/* next element on the ASL */
+    int             *s_semAdd;/* pointer to the semaphore*/
+    pcb_t           *s_procQ; /* tail pointer to a*/
+                            /* process queue*/
+} semd_t;
+
+/*local functions*/
+HIDDEN semd_PTR searchASL (int *semAdd);
+HIDDEN semd_PTR allocASL(int *semAdd);
+/*end of local functions*/
+
+/*globals*/
+HIDDEN semd_PTR semdFree_h;
+HIDDEN semd_PTR semd_h;
+/*end of globals*/
+
+/**
+ * This method initializes the semdFree list to contain all the elements of the array
+ * static semd t semdTable[MAXPROC] This method will be only called once during data structure initialization.
+**/
+void initASL (){
+    int i;
+    static semd_t semdTable[MAXPROC+2]; /* make a table with MAXPROC+2 entries*/
+    semdFree_h = &semdTable[0];
+    for(i=1; i<MAXPROC; i++){/*initialize semdFree List*/
+        semdTable[i-1].s_next = & semdTable[i];
+    }
+    semdTable[MAXPROC-1].s_next = NULL;
+    semd_h = &semdTable[MAXPROC];
+    semd_h->s_next = &semdTable[MAXPROC+1];
+    semd_h->s_next->s_next = NULL;
+    semd_h->s_semAdd =0;/*head of ASL list has semdAdd 0*/
+    semd_h -> s_next ->s_semAdd = MAXINT;/*tail of ASL list has semdAdd near infinity*/
+    semd_h -> s_procQ = mkEmptyProcQ();
+    semd_h->s_next->s_procQ = mkEmptyProcQ();
+    return;
+}
+
+/**
+ * This method searches the active semdList for a semAdd.
+ * Two cases: found -> calls insertProcQ 
+ * Or not found -> allocate new node and put it into the active list then preform found case.
+**/
+int insertBlocked (int *semAdd, pcb_PTR p){
+    semd_PTR ASLPrev = searchASL(semAdd);/*dummy pointer that points to address from find*/
+    if(ASLPrev->s_next->s_semAdd == semAdd){/*if the sem addresses match*/
+        p->p_semAdd = semAdd;
+        insertProcQ(&(ASLPrev->s_next-> s_procQ), p);
+        return FALSE; 
+    }
+    /*if we don't find, remove semdFree*/
+    if(semdFree_h==NULL){/*if the free list is empty, there is an error*/
+        return TRUE;
+    }
+    /*remove semd from Free list and add to ASL, then and insert pcb into new semd */
+    semd_t *newSemd = allocASL(semAdd);
+    semd_t *ASLNext = ASLPrev->s_next;
+    ASLPrev->s_next = newSemd;
+    p->p_semAdd = semAdd;
+    newSemd ->s_next = ASLNext;
+    insertProcQ(&(newSemd->s_procQ), p);
+    return FALSE;/*return false if semdFree not empty*/
+}
+
+
+/**
+ * This is a mutator method searches activeSemd List for matching semdAdd provided. 
+ * Two cases: not found -> error case
+ * Found -> removeProcQ on the process queue that you found in activeSemd list this value is returned
+ * Afterwards, if the processQueue is not empty -> done.
+ * processQueue is empty -> Removes the node out of active list and inserts into the free list.
+**/
+pcb_PTR removeBlocked (int *semAdd){
+    semd_PTR tempSemAdd = searchASL(semAdd);/*dummy pointer that points to address from find*/
+    if(tempSemAdd->s_next->s_semAdd == semAdd){/*Found semd with given semAdd*/
+        pcb_PTR returnP = removeProcQ(&(tempSemAdd->s_next-> s_procQ));
+        if(emptyProcQ(tempSemAdd->s_next -> s_procQ)){/*if process queue empty, put semd back on free list*/
+            semd_PTR tempRemoval = tempSemAdd ->s_next;/*Dummy pointer to what we want to remove*/
+            tempSemAdd ->s_next = tempRemoval->s_next;
+            tempRemoval->s_next = semdFree_h;
+            tempRemoval->s_semAdd = NULL;
+            semdFree_h = tempRemoval;
+        }
+        /*if process queue not empty/we're done adjusting the ASL, then return*/ 
+        return(returnP);
+    }
+    /*if temp and semAdd don't match, then semAdd not in ASL, so error*/
+    return NULL;
+}
+
+/**
+ * This is a mutator method removes the pcb pointed to by p from the process queue associated
+ * with p’s semaphore (p → p semAdd) on the ASL.
+**/
+pcb_PTR outBlocked (pcb_PTR p){
+    semd_PTR tempSemAdd = searchASL(p->p_semAdd);/*dummy pointer that points address from find*/
+    if(tempSemAdd->s_next->s_semAdd == p->p_semAdd){/*if semd that contains p is found*/
+        pcb_PTR returnP = outProcQ(&(tempSemAdd->s_next-> s_procQ),p);/*dummy pointer to return*/
+        if(emptyProcQ(tempSemAdd->s_next -> s_procQ)){/*if queue empty, return semd to free list*/
+            semd_PTR tempRemoval = tempSemAdd ->s_next;/*Dummy pointer that points to semd that we want to put on the free list*/
+            tempSemAdd ->s_next = tempRemoval->s_next;
+            tempRemoval->s_next = semdFree_h;
+            semdFree_h = tempRemoval;
+        }
+        /*once done with semaphore, return p*/
+        return returnP;
+    }
+    /*if semd not on list, error*/
+    return NULL;
+}
+
+/**
+ * This is an accessor method returns a pointer to the pcb that is at the head of the process queue
+ * associated with the semaphore semAdd. Return NULL if semAdd is
+ * not found on the ASL or if the process queue associated with semAdd is empty.
+**/
+pcb_PTR headBlocked (int *semAdd){
+    semd_t *tempSemAdd = searchASL(semAdd);/*dummy pointer that points to address from find*/
+    if(tempSemAdd->s_next->s_semAdd ==semAdd){/*If semd with semAdd is found*/
+        if(emptyProcQ(tempSemAdd->s_next->s_procQ)){/*If semd queue is empty*/
+            return NULL;
+        }
+        return headProcQ(tempSemAdd->s_next->s_procQ);/*If queue not empty return head*/
+    }
+    return NULL;/*If semd not found*/
+}
+
+/**
+ * Cycles through the ASL and finds the closest value to given semAdd.
+**/
+semd_PTR searchASL(int *semAdd){
+    semd_t *temp = semd_h; /*dummy node pointing to the head*/
+    while(temp ->s_next-> s_semAdd < semAdd){/*Finds prev semd in ASL*/
+        temp=temp->s_next;
+    }
+    return temp;/*returns prev*/
+}
+
+/**
+ * Allocates a semd from the semdFree list, assigns it the given semAdd, and creates and empty queue.
+ **/
+semd_PTR allocASL(int *semAdd){
+    semd_PTR newSemd = semdFree_h;/*get new semd from list*/
+    semdFree_h = newSemd->s_next;
+    newSemd ->s_next =NULL;
+    newSemd ->s_semAdd = semAdd;
+    newSemd ->s_procQ = mkEmptyProcQ();
+    return newSemd;
+}
---
+++ phase2/pcb.c
@@ -0,0 +1,269 @@
+/*
+ * This file contains the freePCB list, and methods that helps it maintain and manipulate the queue. 
+ * It also contains the process trees that also organize the pcbs. The methods for this section are used to support 
+ * and manipulate the process trees.
+ * 
+ * 
+ * This code is written by Umang Joshi and Amy Kelley, with help from Mikey G.
+ */
+
+#include "../h/const.h"
+#include "../h/types.h"
+#include "../h/pcb.h"
+#include "../h/asl.h"
+
+/**
+ * Method for debugging for seeing a value or seeing where we are.
+**/
+
+/* Global Variables */
+HIDDEN pcb_PTR pcbFree_h;
+/*End of Global Variables*/
+
+/*--------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------Below: methods for the queue----------------------------------------*/
+/*------------------------------------------------------------------------------------------------------*/
+
+/**
+ * This method inserts the element pointed to by p onto the pcbFree list.
+ * This is done uing the insertProcQ method.
+**/
+void freePcb(pcb_t*p){
+    insertProcQ(&pcbFree_h,p);
+}
+
+/**
+ * Remove an element from the pcbFree list, provide initial values for ALL
+ * of the pcbs fields and then return a pointer to the removed element.
+**/
+pcb_t* allocPcb(){
+    if(pcbFree_h == NULL){/*if the pcbFree list is empty, return NULL*/
+        return NULL;
+    }
+    /*initialize all our pointers*/
+    pcb_t *p=removeProcQ(&pcbFree_h);/*set the pointer to point to the removed pcb*/
+    p->p_next =NULL;
+    p->p_prev =NULL;
+    p->p_semAdd =NULL;
+    p->p_prnt =NULL;
+    p->p_child = NULL;
+    p->p_sib_next = NULL;
+    p->p_sib_prev =NULL;
+    return (p);/*return the pointer*/
+}
+
+/**
+ * Initialize the pcbFree list to contain all the elements of the
+ * static array of MAXPROC pcbs. This method will be called only
+ * once during data structure initialization.
+**/
+void initPcbs(){
+    int i;
+    pcbFree_h = mkEmptyProcQ();
+    static pcb_t foo[MAXPROC];
+    for( i=0; i<MAXPROC; i++){
+        insertProcQ(&pcbFree_h,&foo[i]);
+    }
+}
+
+/**
+ * This method is used to initialize a variable to be tail pointer to a
+ * process queue. Return a pointer to the tail of an empty process queue.
+**/
+pcb_t* mkEmptyProcQ(){
+    return (NULL);
+}
+
+/**
+ * This method returns true if the tp is empty, otherwise: returns false.
+**/
+int emptyProcQ(pcb_t*tp){
+    return(tp == NULL);
+}
+
+/**
+ * Insert the pcb pointed to by p into the process queue whose tail pointer is pointed to by tp. 
+ * Note the double indirection through tp to allow for the possible updating of the tail pointer as well.
+**/
+void insertProcQ(pcb_t* *tp, pcb_t* p){
+    if(emptyProcQ(*tp)==TRUE){ /*if queue is empty...*/
+        p-> p_next = p; /*the head points to what p points to*/
+        p->p_prev =p;
+        (*tp) = p;/*the tail is whatever p point to*/
+    } /*if the queue has one or more element(s) */
+    pcb_t *head = (*tp) -> p_next; /*Dummy pointer to the head of the queue.*/
+    p->p_next = head;
+    head -> p_prev = p;
+    (*tp) -> p_next = p;
+    p -> p_prev = (*tp);
+    (*tp) = p;
+}
+
+/**
+ * This method removes the head from the process queue whose tail-pointer is pointed to by tp. 
+ * Return NULL if the process queue was initially empty; 
+ * otherwise return the pointer to the removed element. Update the process queue’s tail pointer if necessary.
+**/
+pcb_t*removeProcQ(pcb_t**tp){
+    if(emptyProcQ(*tp)==TRUE){/*if there is nothing*/
+        return NULL;
+    }
+    pcb_t *head = (*tp)->p_next; /*dummy pointer to the head*/
+    if(head == (*tp)){/*only 1 item in queue*/
+        (*tp) -> p_next = NULL;
+        (*tp) -> p_prev = NULL;
+        (*tp) = NULL;
+        return (head);
+    }
+    /*if we have more than one thing*/
+    pcb_t *newHead = head ->p_next;/*Dummy pointer to the new head*/
+    head->p_prev = NULL; 
+    (*tp) -> p_next = newHead; 
+    newHead ->p_prev = (*tp);
+    head ->p_next =NULL;
+    return(head);/*return old head*/
+}
+
+/**
+ *Remove the pcb pointed to by p from the process queue whose tailpointer is pointed to by tp. 
+ *Update the process queue’s tail pointer if necessary. 
+ *If the desired entry is not in the indicated queue (an error condition), return NULL; otherwise, return p. 
+**/
+pcb_t*outProcQ(pcb_t* *tp, pcb_t* p){
+    int i;
+    pcb_t *dumTail = (*tp);/*dummy pointer to tail*/
+    if(emptyProcQ(*tp)){ /*if the queue is empty return NULL*/
+        return NULL;
+    }
+    if(dumTail->p_next == p){ /*if the head is the pointer then call removeProcQ on tp*/
+        return removeProcQ(tp);
+    }
+    for(i=0; i<MAXPROC; i++){
+        if(dumTail != p){
+            dumTail= dumTail->p_next;/*recursive call that shifts dumTail*/
+            continue;
+        }
+        pcb_t *forwardTo = dumTail ->p_next;/*Dummy pointer to the next entry*/
+        pcb_t *backwardTo = dumTail ->p_prev;/*Dummy pointer to the prev entry*/
+        backwardTo ->p_next = forwardTo;
+        forwardTo ->p_prev = backwardTo;
+        dumTail->p_next=NULL; 
+        dumTail ->p_prev =NULL;
+        return(dumTail); /*return pointer to removed*/
+    }
+    return(NULL); /*p is not on the list, so NULL*/
+}
+
+/**
+ *Return a pointer to the first pcb from the process queue whose tail
+ *is pointed to by tp. Do not remove this pcb from the process queue.
+ *Return NULL if the process queue is empty.
+**/
+pcb_t*headProcQ(pcb_t*tp){
+    if(emptyProcQ(tp)){
+        return(NULL);
+    }
+    return(tp->p_next);
+}
+
+
+/*--------------------------------------------------------------------------------------------------------*/
+/*---------------------------------Below: methods for process trees--------------------------------------*/
+/*------------------------------------------------------------------------------------------------------*/
+
+
+
+/**
+ *This method returns TRUE if the pcb pointed to by p has no children. Return
+ *FALSE otherwise.
+**/
+int emptyChild(pcb_t *p){
+    return(p->p_child == NULL);
+}
+
+/**
+ * This method makes the pcb pointed to by p a child of the pcb pointed to by prnt.
+**/
+void insertChild(pcb_t *prnt, pcb_t *p){
+    if(emptyChild(prnt)){ /*if no other children, point at new child*/
+        prnt ->p_child = p;
+        p -> p_prnt = prnt;
+        p->p_sib_next = NULL;
+        p->p_sib_prev = NULL;
+        return;
+    }if(!emptyChild(prnt)){/*if there are children: parent points to new child and new child points to sibling + parent.*/
+        pcb_t *currentChild = prnt->p_child;/*dummy pointer to current child*/
+        p->p_sib_prev = currentChild;
+        p->p_next = NULL;
+        currentChild ->p_sib_next = p;
+        prnt ->p_child = p;
+        p->p_prnt = prnt;
+        return;
+    } 
+}
+
+/**
+ * This method makes the first child of the pcb pointed to by p no longer a child of
+ * p. Return NULL if initially there were no children of p. Otherwise,
+ * return a pointer to this removed first child pcb.
+**/
+pcb_t* removeChild(pcb_t *p){
+    if(emptyChild(p)){/*call emptyChild to see if there are children*/
+        return NULL;
+    }
+    
+    if(p->p_child->p_sib_prev == NULL){/*if there is one child*/
+        pcb_t *temp = p->p_child;/*dummy pointer to the first child*/
+        p->p_child =NULL;
+        temp ->p_prnt= NULL;
+        return(temp);
+    }
+    /*more than one child*/
+    pcb_t *removeFirst = p->p_child;/*dummy pointer that points to child we want to remove*/
+    pcb_t *firstSib = removeFirst->p_sib_prev;/*dummy pointer to the next sibling*/
+    firstSib ->p_sib_next =NULL;
+    p ->p_child = firstSib; 
+    removeFirst->p_sib_next =NULL; 
+    removeFirst ->p_sib_prev =NULL;
+    removeFirst->p_prnt =NULL; 
+    return(removeFirst); /*return removed child*/
+    
+}
+
+/**
+ * This method make the pcb pointed to by p no longer the child of its parent.
+ * If the pcb pointed to by p has no parent, return NULL; otherwise, return p.
+**/
+pcb_t* outChild(pcb_t *p){
+    if (p == NULL){
+        return NULL;
+    }
+    if(p->p_prnt ==NULL){ /*if you don't have a parent then you are already an orphan*/
+        return NULL;
+    }
+    pcb_t *parent = p->p_prnt;/*dummy pointer to the parent*/
+    if(p == parent->p_child){/*if you are the first child*/
+        return(removeChild(parent));
+    }
+    if(p->p_sib_prev == NULL && p->p_sib_next == NULL && p == p->p_prnt->p_child){/*if you are only child*/
+        return(removeChild(parent));
+    }
+    if(p!= parent->p_child){/*not the first child*/
+        if(p->p_sib_prev ==NULL){/*you are an end child, so no prev sibling*/
+            p->p_prnt=NULL;
+            pcb_t *nextSib = p->p_sib_next;/*dummy pointer to next sibling*/
+            nextSib ->p_sib_prev = NULL;
+            p->p_sib_next = NULL;
+            return(p); /*return child*/
+        }/*if you are a middle child, so have sib_next and sib_prev*/
+        p->p_prnt = NULL;
+        pcb_t *prevSib = p->p_sib_prev;/*dummy pointer to previous sibling*/
+        pcb_t *nextSib = p->p_sib_next;/*dummy pointer to next sibling*/
+        prevSib ->p_sib_next = nextSib;
+        nextSib ->p_sib_prev = prevSib;
+        p->p_sib_next =NULL;
+        p->p_sib_prev =NULL;
+        return(p); /*return orphaned child*/
+    }
+    return NULL;/*case if p doesn't belong*/
+}
