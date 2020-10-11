dc60ee0b59ea581381dc14882324a29dc2d7bdd0
joshiu <joshiu@xavier.edu>
2020-10-11 14:23:08 -0400

    significant changes to main

--- phase2/inital.c
+++ phase2/inital.c
@@ -20,7 +20,9 @@ extern void test(); /*sets up the nucleus calls that are to be tested*/
 
 HIDDEN void generalExceptHandler(); /* method for handling exceptions*/
 
+extern memaddr getRAMTOP(); /*method to get the top of RAM THIS GOES IN .H file*/
 /* GLOBAL VARIABLES */
+
 unsigned int saveState[DEVCNT+DEVPERINT]; /* this is where we save the state when IO*/
 int processCnt; /*number of pcbs that have been allocated*/
 int softBlockCnt; /*number of processes that have been blocked*/
@@ -29,20 +31,19 @@ pcb_t *currentProc; /*process that is currently running*/
 int devSema4[DEVCNT+DEVPERINT+1]; /*array of device semaphores*/
 cpu_t startTimeOD; /*beginning of a time unit*/
 cpu_t timeSlice; /*amount of time until the time slice*/
+#define clockSem devSema4[DEVCNT+DEVPERINT]; /*clock sema4*/
 
 /* END GLOBAL VARIABLES*/
 
+/*--------------------------------------------------------------------------------------------------*/
 
-
-
-/**
- * Insert what this does here
- * */
 int main(){
     passupvector_t *passUpVec;
     int counter;
+    pcb_t *newPcb;
+    memaddr topRAM;
 
-    passUpVec = PASSUPVECTOR; /*a pointer that points to the PASSUPVECTOR*/
+    passUpVec = (passupvector_t *) PASSUPVECTOR; /*a pointer that points to the PASSUPVECTOR*/
 
     passUpVec-> tlb_refll_handler = (memaddr) uTLB_RefillHandler;
     passUpVec -> tlb_refll_stackPtr = 0x20001000;
@@ -58,33 +59,35 @@ int main(){
     currentProc = NULL;
 
     /*loop that initializes all devicesema4s to 0 (idk if this needed)*/
-    for(counter = 0; counter < 49; counter ++){
+    clockSem = 0;
+    for(counter = 0; counter < (DEVCNT+DEVPERINT); counter ++){
         devSema4[counter] = 0;
     }
 
     LDIT(100); /*load interval timer with 100 ms*/
     /*Need to get top of RAM address*/
+    topRAM = getRAMTOP(topRAM);
+    newPcb = allocPcb();
+    if(newPcb!=NULL){
+        newPcb->p_s.s_pc = (memaddr) test;
+        newPcb -> p_s.s_t9 = (memaddr) test;
+        newPcb->p_s.s_status = ; /* figure out how to turn on timer, interrupts, and keep kernel mode*/
+        newPcb->p_time = 0;
+        newPcb->p_supportStruct = NULL;
+        processCnt += 1;
+        insertProcQ(&readyQ, newPcb);
 
-    pcb_t *newPcb = allocPcb();
-    
-    /* we need to set the state such that interrupts, PLT, kernel mode enabled*/
-    /* also need to set SP to RAMTOP and PC to test*/
-
-    // newPcb ->p_s.s_pc = &(test());/*set PC*/
-    // newPcb -> p_s.s_t9 = &(test());/*for technical reasons, we do this when we set the pc*/
-    // newPcb -> p_s.s_TE = 1; /*timer enable bit*/
-    // newPcb ->p_s.s_sp = RAMTOP; /*stack pointer*/
-    // newPcb -> p_s.s_KUp = 0;/*enable kernel mode*/
-    // newPcb ->p_s.s_IEp = 1; /*enable interrupts*/
-    
-    newPcb->p_time = 0;
-    newPcb->p_supportStruct = NULL;
-    insertProcQ(&readyQ, newPcb);
-    processCnt ++;
-    scheduler();
-    /*Need a return?*/
+        scheduler();
+    }
+    else{
+        PANIC();
+    }
+    return (0);
 }
 
+void generalExceptHandler(){
+
+}
 
 /**
  * Insert method comment here
@@ -94,31 +97,7 @@ extern void uTLB_RefillHandler(){
     for example: if it's sysexception then call sys Trap
     so get cause and branch to the correct one needed */
 }
-
-/**
- * idk if we need any of this comment (didn't wanna delete it just in case)
- * extern void uTLB_RefillHandler();
- * something to populate passup at 0x0FFF.F900
- * xxx->tlb refll handler = (memaddr) uTLB RefillHandler;
- *
- * set stack pointer for nucleus TLB refill event to the top of the nucleus event: Ox2000.1000
- * set xxx-> expection_handler = (memaddr) fooBar; (change foo name)
- * set stack pointer for the expection handler to top of Nucleus 0x2000.1000
- * 
- * initilzate all the nucleus maintained variables
- * set system wide interval timer at 1000 millsecs
- * 
- * pcb_t newPcb = allocPCB();
- * newPcb -> p_time = 0;
- * newPcb -> p_semAdd = newPcb->s_a0;
- * newPcb -> p_supportstruct = NULL;
- * 
- * insertProcQ(readyQ, newPcb);
- * processcnt ++;
- * init allocate state on PCB (interrupts enable, process local timer enabled, kernel mode on)
- * SP = RAMTOP
- * PCADDRESS = TEST
- * 
- * 
- * after this never return to main again
- * */
+memaddr getRAMTOP(memaddr t){
+    t = ((* ((int *) RAMBASEADDR))+ (* ((int *) RAMBASESIZE)));
+    return t;
+}
