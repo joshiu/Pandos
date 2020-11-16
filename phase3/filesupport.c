732e9c11b5f714f82cf912884c4fd7bba8acd129
Amy Kelley <amykelley2222@yahoo.com>
2020-11-16 15:05:01 -0500

    added last files for p3

---
+++ phase3/filesupport.c
@@ -0,0 +1,10 @@
+#include "/usr/local/include/umps3/umps/libumps.h"
+#include "../h/const.h"
+#include "../h/types.h"
+#include "../h/initial.h"
+
+/**
+ * This file 
+ * 
+ * Written by Umang Joshi and Amy Kelley
+ * */
