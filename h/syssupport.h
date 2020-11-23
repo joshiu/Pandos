aa0f6a28b66e41204d006dcd67862c56af114c62
joshiu <joshiu@xavier.edu>
2020-11-23 12:10:45 -0500

    changed syssupport

--- h/syssupport.h
+++ h/syssupport.h
@@ -13,6 +13,10 @@
 extern void userGeneralExceptHandler();
 extern void block(int *semAddr);
 extern void unblock(int *semAddr);
+extern void userLoadState(state_t *loadState);
+extern void enableInterrupts(int turnON);
+extern void killProc(int *semAddr);
+
 
 
 #endif
--- phase3/syssupport.c
+++ phase3/syssupport.c
@@ -17,11 +17,12 @@
 /* NOTE: (READ ME): I know all of these aren't void and they don't have have the right 
 pramaters but I wanted to set everything up or maybe i just fucked everything up idk*/
 HIDDEN void userSyscall();
+HIDDEN void uProgramTrap(support_t *supportInfo);
 HIDDEN void sys_9();
 HIDDEN void sys_10();
-HIDDEN void sys_11(support_t *supportStruct);
-HIDDEN void sys_12(support_t *supportStruct);
-HIDDEN void sys_13(support_t *supportStruct);
+HIDDEN void sys_11(support_t *supportInfo);
+HIDDEN void sys_12(support_t *supportInfo);
+HIDDEN void sys_13(support_t *supportInfo);
 
 /**end of file specific methods**/
 
@@ -113,6 +114,7 @@ void userLoadState(state_t *loadState){
     LDST(loadState);
 
 }
+
 /**
  * Not sure this is needed but just in case (method comment here)
  * We also may need a new name for this, but I wasn't sure if it mattered
@@ -120,7 +122,6 @@ void userLoadState(state_t *loadState){
 void userSyscall(support_t *supportInfo){
     /*local variables*/
     int uSysNum; 
-    cpu_t endTime;
     /*end local variables*/
 
     uSysNum = supportInfo ->sup_exceptState[GENERALEXCEPT].s_a0;
@@ -128,12 +129,12 @@ void userSyscall(support_t *supportInfo){
     switch (uSysNum)
     {
     case TERMINATE: {
-        sys_9(NULL);
+        sys_9();
         break;
     }
 
     case GETTOD: {
-        sys_10();
+        sys_10(supportInfo);
         break;
     }
 
@@ -155,6 +156,8 @@ void userSyscall(support_t *supportInfo){
     default: {
         killProc(NULL);
         break;
+    }
+    
     }/*end of switch statement*/
 
     supportInfo ->sup_exceptState[GENERALEXCEPT].s_pc = supportInfo->sup_exceptState[GENERALEXCEPT];
@@ -171,6 +174,7 @@ void sys_9(){
 
     /*READ ME: I understand what to do here, (similar to sys2) but I'm not 100% sure on the new
     U-proc delcarations and everything. Which also make doing sys 10 weird*/
+    killProc(NULL);
 
 }
 
@@ -179,16 +183,15 @@ void sys_9(){
  * microseconds since the system was last booted/reset
  * and places it in UProc's v0
  * */
-void sys_10(){
+void sys_10(support_t *supportInfo){
     
     /*local variables*/
-    cpu_t timeStarted; /*time the system was last booted/reset
+    cpu_t timeEnded; /*time the system was last booted/reset
     /*end of local variables*/
 
-    STCK(timeStarted); /*is this how we read the clock for UProc? or is there something else?*/
+    STCK(timeEnded); /*is this how we read the clock for UProc? or is there something else?*/
 
-    currentProc->p_s.s_v0 = timeStarted;
-    loadState(currentProc);
+    supportInfo->sup_exceptState[GENERALEXCEPT].s_v0 = timeEnded;
 }
 
 /**
@@ -196,8 +199,55 @@ void sys_10(){
  * a line of output (string of characters) has been transmitted to the printer device
  * associated with the U-proc
  * */
-void sys_11(support_t *supportStruct){
+void sys_11(support_t *supportInfo){
+    /*local variables*/
+    int counter;
+    int length;
+    int status;
+    int devSema4Num;
+    int idNum;
+    int error;
+    char *letterToPrint;
+    devregarea_t *deviceRegister;
+    /*end local variables*/
 
+    idNum = supportInfo->sup_asid;
+
+    deviceRegister = (devregarea_t *) RAMBASEADDR;
+    devSema4Num = ((PRNTINT-DISKINT) *DEVPERINT) +(idNum - 1);
+    
+    letterToPrint = (char *)supportInfo->sup_exceptState[GENERALEXCEPT].s_a1;
+    length = supportInfo->sup_exceptState[GENERALEXCEPT].s_a2;
+
+    if(((int)letterToPrint < KUSEG) || (length<=0) || (length >WORDLEN)){
+        killProc(NULL);
+    }
+
+    block(&globalDevSemReg[devSema4Num]);
+
+    counter = 0;
+    error = FALSE;
+
+    while((counter<length) && (!error)){
+
+        deviceRegister->devreg[devSema4Num].d_data0 = *letterToPrint;
+        deviceRegister -> devreg[devSema4Num].d_command = TRANSMITCHAR;
+        status = SYSCALL(WAITIO, PRNTINT, idNum-1, 0);
+
+        if(status != READY){
+            error = TRUE;
+        }
+        else{
+            counter+=1;
+        }
+
+        letterToPrint++;
+    }
+
+    unblock(&globalDevSemReg[devSema4Num]);
+
+    /*how many characters were printed*/
+    supportInfo -> sup_exceptState[GENERALEXCEPT].s_v0 = counter; 
 }
 
 
@@ -206,8 +256,63 @@ void sys_11(support_t *supportStruct){
  * a line of output (string of characters) has been transmitted to the terminal device
  * associated with the U-proc
  * */
-void sys_12(support_t *supportStruct){
+void sys_12(support_t *supportInfo){
+    /*local variables*/
+    int counter;
+    int length;
+    int status;
+    int devSema4Num;
+    int idNum;
+    int error;
+    char *letterToPrint;
+    devregarea_t *deviceRegister;
+    /*end local variables*/
 
+    idNum = supportInfo->sup_asid;
+
+    deviceRegister = (devregarea_t *) RAMBASEADDR;
+    devSema4Num = ((TERMINT-DISKINT) *DEVPERINT) +(idNum - 1);
+    
+    letterToPrint = (char *)supportInfo->sup_exceptState[GENERALEXCEPT].s_a1;
+    length = supportInfo->sup_exceptState[GENERALEXCEPT].s_a2;
+
+    if(((int)letterToPrint < KUSEG) || (length<=0) || (length >WORDLEN)){
+        killProc(NULL);
+    }
+
+    block(&globalDevSemReg[devSema4Num]);
+
+    counter = 0;
+    error = FALSE;
+
+    while((counter<length) && (!error)){
+        /*can we do 2 separate here*/
+
+        deviceRegister->devreg[devSema4Num].t_transm_command = *letterToPrint <<BYTELENGTH |TRANSMITCHAR;
+        
+        deviceRegister->devreg[devSema4Num].d_data0 = *letterToPrint;
+        deviceRegister -> devreg[devSema4Num].d_command = TRANSMITCHAR;
+        
+        status = SYSCALL(WAITIO, PRNTINT, idNum-1, 0);
+
+        if((status & 0xFF) != OKCHARTRANS){
+            error = TRUE;
+        }
+        else{
+            counter+=1;
+        }
+
+        letterToPrint++;
+    }
+
+    unblock(&globalDevSemReg[devSema4Num]);
+
+    if(error){
+        counter = 0 - (status & 0xFF);
+    }
+
+    /*how many characters were printed*/
+    supportInfo -> sup_exceptState[GENERALEXCEPT].s_v0 = counter; 
 }
 
 
@@ -216,6 +321,62 @@ void sys_12(support_t *supportStruct){
  *  until a line of input (string of characters) has been transmitted from the 
  * terminal device associated with the U-proc.
  * */
-void sys_13(support_t *supportStruct){
+void sys_13(support_t *supportInfo){
+    /*local variables*/
+    int counter;
+    int status;
+    int done;
+    int devSema4Num;
+    int idNum;
+    int error;
+    char *letterToPrint;
+    devregarea_t *deviceRegister;
+    /*end local variables*/
+
+    idNum = supportInfo->sup_asid;
+
+    deviceRegister = (devregarea_t *) RAMBASEADDR;
+    devSema4Num = ((TERMINT-DISKINT) *DEVPERINT) +(idNum - 1);
+    
+    letterToPrint = (char *)supportInfo->sup_exceptState[GENERALEXCEPT].s_a1;
+
+    if(((int)letterToPrint < KUSEG)){
+        killProc(NULL);
+    }
+
+    block(&globalDevSemReg[devSema4Num +DEVPERINT]);
+
+    counter = 0;
+    error = FALSE;
+    done = FALSE;
+
+    while((!done) && (!error)){
+        /*can we do 2 separate here*/
+
+        deviceRegister ->devreg[devSema4Num].t_recv_command = TRANSMITCHAR;
+        
+        status = SYSCALL(WAITIO, TERMINT, idNum-1, TRUE);
+
+        if((status & 0xFF) != OKCHARTRANS){
+            error = TRUE;
+        }
+        else{
+            counter+=1;
+            *letterToPrint = status >>8;
+            letterToPrint++;
+            if((status>>8) == 0x0A){
+                done =TRUE;
+            }
+        }
+    }
+
+    unblock(&globalDevSemReg[devSema4Num+DEVPERINT]);
+
+    if(error){
+        counter = 0 - (status & 0xFF);
+    }
+
+    /*how many characters were printed*/
+    supportInfo -> sup_exceptState[GENERALEXCEPT].s_v0 = counter; 
 
 }
